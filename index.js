876bacb62b5bb65bc7590d2c21e3cf32e8648b25
andressajuli-espec <andressajuli@gmail.com>
2026-01-04 14:33:30 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+const verifyToken = process.env.VERIFY_TOKEN;
+
+app.get('/', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode === 'subscribe' && token === verifyToken) {
+    console.log('WEBHOOK VERIFIED');
+    return res.status(200).send(challenge);
+  }
+
+  return res.sendStatus(403);
+});
+
+app.post('/', (req, res) => {
+  console.log('Webhook recebido:');
+  console.log(JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
